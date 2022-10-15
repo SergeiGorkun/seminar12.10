78bd3302ac99937f083abf46c8c1dc150aa92e32
SergeiGorkun <sergeigorkun1992@yandex.ru>
2022-10-15 22:08:24 +0600

    7

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -1,2 +1,16 @@
 ﻿// В указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
-Console.WriteLine("Hello, World!");
+double [] numbers = {1, 2, -4, 5, 6.245, 12/7, 0.1};
+double Min = numbers [0];
+double Max = numbers [0];
+
+for(int i = 0; i < numbers.Length; i++){
+    if(Min > numbers[i]){
+        Min = numbers[i];
+    }
+    if(Max < numbers[i]){
+        Max = numbers[i];
+    }
+}
+double Ras = Max - Min;     
+Console.WriteLine(Ras);
+
